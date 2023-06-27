6be51e018446ad20ded630710af4ede933a98de8
THIAGO PIRES DO NASCIMENTO <thiago.pnascimento9@senacsp.edu.br>
2023-06-27 09:07:18 -0300

    teste

--- CatalagoFilmes/View/TelaCadastroSerie.Designer.cs
+++ CatalagoFilmes/View/TelaCadastroSerie.Designer.cs
@@ -42,7 +42,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(235, 203);
+            this.button1.Location = new System.Drawing.Point(209, 188);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(121, 90);
             this.button1.TabIndex = 19;
@@ -52,7 +52,7 @@
             // 
             // txBoxTemporadas
             // 
-            this.txBoxTemporadas.Location = new System.Drawing.Point(77, 235);
+            this.txBoxTemporadas.Location = new System.Drawing.Point(51, 220);
             this.txBoxTemporadas.Name = "txBoxTemporadas";
             this.txBoxTemporadas.Size = new System.Drawing.Size(100, 23);
             this.txBoxTemporadas.TabIndex = 18;
@@ -70,21 +70,21 @@
             "Ação",
             "Suspense",
             "Drama"});
-            this.cBoxCategoria.Location = new System.Drawing.Point(235, 167);
+            this.cBoxCategoria.Location = new System.Drawing.Point(209, 152);
             this.cBoxCategoria.Name = "cBoxCategoria";
             this.cBoxCategoria.Size = new System.Drawing.Size(121, 23);
             this.cBoxCategoria.TabIndex = 17;
             // 
             // txBoxEstreia
             // 
-            this.txBoxEstreia.Location = new System.Drawing.Point(77, 167);
+            this.txBoxEstreia.Location = new System.Drawing.Point(51, 152);
             this.txBoxEstreia.Name = "txBoxEstreia";
             this.txBoxEstreia.Size = new System.Drawing.Size(106, 23);
             this.txBoxEstreia.TabIndex = 16;
             // 
             // txBoxSerie
             // 
-            this.txBoxSerie.Location = new System.Drawing.Point(77, 104);
+            this.txBoxSerie.Location = new System.Drawing.Point(51, 89);
             this.txBoxSerie.Name = "txBoxSerie";
             this.txBoxSerie.Size = new System.Drawing.Size(279, 23);
             this.txBoxSerie.TabIndex = 15;
@@ -92,7 +92,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(77, 217);
+            this.label5.Location = new System.Drawing.Point(51, 202);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(74, 15);
             this.label5.TabIndex = 14;
@@ -101,7 +101,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(235, 149);
+            this.label4.Location = new System.Drawing.Point(209, 134);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(61, 15);
             this.label4.TabIndex = 13;
@@ -110,7 +110,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(77, 149);
+            this.label3.Location = new System.Drawing.Point(51, 134);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 15);
             this.label3.TabIndex = 12;
@@ -119,7 +119,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(77, 86);
+            this.label2.Location = new System.Drawing.Point(51, 71);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 15);
             this.label2.TabIndex = 11;
@@ -129,7 +129,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(135, 36);
+            this.label1.Location = new System.Drawing.Point(109, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(167, 25);
             this.label1.TabIndex = 10;
@@ -139,7 +139,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(427, 309);
+            this.ClientSize = new System.Drawing.Size(371, 299);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txBoxTemporadas);
             this.Controls.Add(this.cBoxCategoria);
@@ -151,7 +151,8 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "TelaCadastroSerie";
-            this.Text = "TelaCadastroSerie";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro Série";
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- CatalagoFilmes/View/TelaMenu.Designer.cs
+++ CatalagoFilmes/View/TelaMenu.Designer.cs
@@ -60,7 +60,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(184, 10);
+            this.label1.Location = new System.Drawing.Point(181, 32);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(217, 30);
             this.label1.TabIndex = 2;
@@ -88,7 +88,8 @@
             this.MaximizeBox = false;
             this.Name = "TelaMenu";
             this.ShowIcon = false;
-            this.Text = "TelaMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tela Escolha";
             this.ResumeLayout(false);
             this.PerformLayout();
 
